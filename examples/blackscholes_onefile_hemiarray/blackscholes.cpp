8e3063a40eef67e4c9e4f54387f277d5c5464c2d
Mark Harris <harrism@gmail.com>
2012-10-10 13:54:07 +1000

    Added single-file black-scholes example that uses hemi::Array to simplify data management

---
+++ examples/blackscholes_onefile_hemiarray/blackscholes.cpp
@@ -0,0 +1,144 @@
+///////////////////////////////////////////////////////////////////////////////
+// This is a simple example that performs a Black-Scholes options pricing
+// calculation using code that is almost entirely shared between host (CPU)
+// code compiled with any C/C++ compiler (including NVCC) and device code
+// that is compiled with the NVIDIA CUDA compiler, NVCC. Note there are only
+// 25 lines of code in the single .cu file in this example. The majority of 
+// the computational code, which is in black_scholes_shared.h, is shared 
+// between host and device compilers.
+///////////////////////////////////////////////////////////////////////////////
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>
+#include <cuda_runtime_api.h>
+
+#include "timer.h"
+#include "hemi.h"
+#include "array.h"
+
+const float      RISKFREE = 0.02f;
+const float    VOLATILITY = 0.30f;
+
+///////////////////////////////////////////////////////////////////////////////
+// Polynomial approximation of cumulative normal distribution function
+///////////////////////////////////////////////////////////////////////////////
+HEMI_DEV_CALLABLE_INLINE
+float CND(float d)
+{
+    const float       A1 = 0.31938153f;
+    const float       A2 = -0.356563782f;
+    const float       A3 = 1.781477937f;
+    const float       A4 = -1.821255978f;
+    const float       A5 = 1.330274429f;
+    const float RSQRT2PI = 0.39894228040143267793994605993438f;
+
+    float
+        K = 1.0f / (1.0f + 0.2316419f * fabsf(d));
+
+    float
+        cnd = RSQRT2PI * expf(-0.5f * d * d) * 
+        (K * (A1 + K * (A2 + K * (A3 + K * (A4 + K * A5)))));
+
+    if(d > 0)
+        cnd = 1.0f - cnd;
+
+    return cnd;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Black-Scholes formula for both call and put
+///////////////////////////////////////////////////////////////////////////////
+HEMI_KERNEL(BlackScholes)
+    (float *callResult, float *putResult, const float *stockPrice,
+     const float *optionStrike, const float *optionYears, float Riskfree,
+     float Volatility, int optN)
+{
+    int offset = hemiGetElementOffset();
+    int stride = hemiGetElementStride();
+
+    for(int opt = offset; opt < optN; opt += stride)
+    {
+        float S = stockPrice[opt];
+        float X = optionStrike[opt];
+        float T = optionYears[opt]; 
+        float R = Riskfree;
+        float V = Volatility;
+
+        float sqrtT = sqrtf(T);
+        float    d1 = (logf(S / X) + (R + 0.5f * V * V) * T) / (V * sqrtT);
+        float    d2 = d1 - V * sqrtT;
+        float CNDD1 = CND(d1);
+        float CNDD2 = CND(d2);
+
+        //Calculate Call and Put simultaneously
+        float expRT = expf(- R * T);
+        callResult[opt] = S * CNDD1 - X * expRT * CNDD2;
+        putResult[opt]  = X * expRT * (1.0f - CNDD2) - S * (1.0f - CNDD1);
+    }
+}
+
+float RandFloat(float low, float high){
+    float t = (float)rand() / (float)RAND_MAX;
+    return (1.0f - t) * low + t * high;
+}
+
+int main(int argc, char **argv)
+{
+    int OPT_N  = 4000000;
+    int OPT_SZ = OPT_N * sizeof(float);
+
+    printf("Initializing data...\n");
+
+    hemi::Array<float> callResult(OPT_SZ, true);
+    hemi::Array<float> putResult(OPT_SZ, true);
+    hemi::Array<float> stockPrice(OPT_SZ, true);
+    hemi::Array<float> optionStrike(OPT_SZ, true);
+    hemi::Array<float> optionYears(OPT_SZ, true);
+        
+    srand(5347);
+    //Generate options set
+    for (int i = 0; i < OPT_N; i++) {
+        callResult.getWriteOnlyHostPtr()[i]    = 0.0f;
+        putResult.getWriteOnlyHostPtr()[i]     = -1.0f;
+        stockPrice.getWriteOnlyHostPtr()[i]    = RandFloat(5.0f, 30.0f);
+        optionStrike.getWriteOnlyHostPtr()[i]  = RandFloat(1.0f, 100.0f);
+        optionYears.getWriteOnlyHostPtr()[i]   = RandFloat(0.25f, 10.0f);
+    }
+    
+    int blockDim = 128; // ignored by host code
+    int gridDim  = std::min(1024, (OPT_SZ + blockDim - 1) / blockDim);
+
+#ifdef HEMI_CUDA_COMPILER 
+    printf("Running GPU Version...\n");
+    hemi::Location loc = hemi::device;
+#else
+    printf("Running CPU Version...\n");
+    hemi::Location loc = hemi::host;
+#endif
+
+    StartTimer();
+    float *d_callResult         = callResult.getWriteOnlyPtr(loc);
+    float *d_putResult          = putResult.getWriteOnlyPtr(loc);
+    const float *d_stockPrice   = stockPrice.getReadOnlyPtr(loc);
+    const float *d_optionStrike = optionStrike.getReadOnlyPtr(loc);
+    const float *d_optionYears  = optionYears.getReadOnlyPtr(loc);        
+
+    HEMI_KERNEL_LAUNCH(BlackScholes, gridDim, blockDim,
+                       d_callResult, d_putResult, d_stockPrice, d_optionStrike, 
+                       d_optionYears, RISKFREE, VOLATILITY, OPT_N);
+
+    // force copy back to host if needed and print a sanity check
+    printf("Option 0 call: %f\n", callResult.getReadOnlyPtr(hemi::host)[0]); 
+    printf("Option 0 put:  %f\n", putResult.getReadOnlyPtr(hemi::host)[0]);
+
+    double ms = GetTimer();
+
+    //Both call and put is calculated
+    printf("Options count             : %i     \n", 2 * OPT_N);
+    printf("\tBlackScholes() time    : %f msec\n", ms);
+    printf("\t%f GB/s, %f GOptions/s\n", 
+           ((double)(5 * OPT_N * sizeof(float)) * 1E-9) / (ms * 1E-3),
+           ((double)(2 * OPT_N) * 1E-9) / (ms * 1E-3));
+}
