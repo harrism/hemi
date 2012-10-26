da61c59880b371a306f8fc1bc8ec5dced8d4871c
Mark Harris <harrism@gmail.com>
2012-10-26 17:13:20 +1000

    Added nbody.h to MSVC project. Fixed linker error in nbody.

--- examples/nbody_vec4/nbody.h
+++ examples/nbody_vec4/nbody.h
@@ -1,8 +1,5 @@
 #include "hemi/hemi.h"
 
-// Softening constant prevents division by zero
-HEMI_DEFINE_CONSTANT(float softeningSquared, 0.01f);
-
 // Compute gravitational force between two bodies.
 // Body mass is stored in w component of the Vec4f.
 HEMI_DEV_CALLABLE
--- examples/nbody_vec4/nbody_vec4.cu
+++ examples/nbody_vec4/nbody_vec4.cu
@@ -2,6 +2,9 @@
 #include "nbody.h"
 #include <stdio.h>
 
+// Softening constant prevents division by zero
+HEMI_DEFINE_CONSTANT(float softeningSquared, 0.01f);
+
 // Compute gravitational force between two bodies.
 // Body mass is stored in w component of the Vec4f.
 HEMI_DEV_CALLABLE
