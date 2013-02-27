e33a13172be4cc229ec67aa88ecf1760a0d44743
Mark Harris <harrism@gmail.com>
2013-02-27 15:20:11 +1000

    Add support for preallocated host pointers, and ability to copy to/from an external host pointer.

--- hemi/array.h
+++ hemi/array.h
@@ -1,4 +1,5 @@
 #include "hemi/hemi.h"
+#include <cstring>
 
 #ifndef HEMI_ARRAY_DEFAULT_LOCATION
   #ifdef HEMI_CUDA_COMPILER
@@ -25,6 +26,7 @@ namespace hemi {
           nSize(n), 
           hPtr(0),
           dPtr(0),
+          isForeignHostPtr(false),
           isPinned(usePinned),
           isHostAlloced(false),
           isDeviceAlloced(false),
@@ -34,11 +36,70 @@ namespace hemi {
             allocateHost();
         }
 
+        // Use a pre-allocated host pointer (use carefully!)
+        Array(T *hostMem, size_t n) : 
+          nSize(n), 
+          hPtr(hostMem),
+          dPtr(0),
+          isForeignHostPtr(true),
+          isPinned(false),
+          isHostAlloced(true),
+          isDeviceAlloced(false),
+          isHostValid(true),
+          isDeviceValid(false) 
+        {
+        }
+
         ~Array() 
         {           
             deallocateDevice();
-            deallocateHost();
+            if (!isForeignHostPtr)
+                deallocateHost();
         }
+
+        size_t size() const { return nSize; }
+
+        // copy from/to raw external pointers (host or device)
+
+        void copyFromHost(const T *other, size_t n)
+        {
+            if ((isHostAlloced || isDeviceAlloced) && nSize != n) {
+                deallocateHost();
+                deallocateDevice();
+                nSize = n;
+                allocateHost();
+            }
+            memcpy(writeOnlyHostPtr(), other, nSize * sizeof(T));
+        }
+
+        void copyToHost(T *other, size_t n) const
+        {
+            assert(isHostAlloced);
+            assert(n <= nSize);            
+            memcpy(other, readOnlyHostPtr(), n * sizeof(T));
+        }            
+
+#ifndef HEMI_CUDA_DISABLE
+        void copyFromDevice(const T *other, size_t n)
+        {
+            if ((isHostAlloced || isDeviceAlloced) && nSize != n) {
+                deallocateHost();
+                deallocateDevice();
+                nSize = n;
+                allocateDevice();
+            }
+            checkCuda( cudaMemcpy(writeOnlyDevicePtr(), other, 
+                                  nSize * sizeof(T), cudaMemcpyDeviceToDevice) );
+        }
+
+        void copyToDevice(T *other, size_t n)
+        {
+            assert(isDeviceAlloced);
+            assert(n <= nSize);
+            checkCuda( cudaMemcpy(other, readOnlyDevicePtr(), 
+                                  nSize * sizeof(T), cudaMemcpyDeviceToDevice) );
+        }
+#endif
 
         // read/write pointer access
 
@@ -115,6 +176,7 @@ namespace hemi {
     private:
         size_t          nSize;
 
+        bool            isForeignHostPtr;
         bool            isPinned;
         
         mutable T       *hPtr;
@@ -131,29 +193,34 @@ namespace hemi {
         {
             assert(!isHostAlloced);
             assert(!isDeviceAlloced);
-
+#ifndef HEMI_CUDA_DISABLE
             if (isPinned)
                 checkCuda( cudaHostAlloc((void**)&hPtr, nSize * sizeof(T), 0));
             else
+#endif
                 hPtr = new T[nSize];    
                 
             isHostAlloced = true;
-            isHostValid   = true;
+
         }
         
         void allocateDevice() const
         {
+#ifndef HEMI_CUDA_DISABLE
             assert(!isDeviceAlloced);
             checkCuda( cudaMalloc((void**)&dPtr, nSize * sizeof(T)) );
             isDeviceAlloced = true;
+#endif
         }
 
         void deallocateHost()
         {
             if (isHostAlloced) {
+#ifndef HEMI_CUDA_DISABLE
                 if (isPinned)
                     checkCuda( cudaFreeHost(hPtr) );
                 else
+#endif
                     delete [] hPtr;
                 nSize = 0;
                 isHostAlloced = false;
@@ -163,15 +230,18 @@ namespace hemi {
 
         void deallocateDevice()
         {
+#ifndef HEMI_CUDA_DISABLE
             if (isDeviceAlloced) {
                 checkCuda( cudaFree(dPtr) );
                 isDeviceAlloced = false;
                 isDeviceValid   = false;
             }
+#endif
         }
 
         void copyHostToDevice() const
         {
+#ifndef HEMI_CUDA_DISABLE
             assert(isHostAlloced);
             if (!isDeviceAlloced) allocateDevice();
             checkCuda( cudaMemcpy(dPtr, 
@@ -179,16 +249,19 @@ namespace hemi {
                                   nSize * sizeof(T), 
                                   cudaMemcpyHostToDevice) );
             isDeviceValid = true;
+#endif
         }
 
         void copyDeviceToHost() const
         {
+#ifndef HEMI_CUDA_DISABLE
             assert(isDeviceAlloced && isHostAlloced);
             checkCuda( cudaMemcpy(hPtr, 
                                   dPtr, 
                                   nSize * sizeof(T), 
                                   cudaMemcpyDeviceToHost) );
             isHostValid = true;
+#endif
         }
         
     };
