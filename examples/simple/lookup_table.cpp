e1443c176710abfdba51d56c8361312da4b73afb
rcalland <richard.calland@ipmu.jp>
2016-07-17 16:20:25 +0900

    adding example program (not working yet)

---
+++ examples/simple/lookup_table.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "hemi/hemi.h"
+#include "hemi/table.h"
+#include "hemi/launch.h"
+#include "hemi/device_api.h"
+
+int main(void) {
+
+  hemi::Table3D<float> lookup_table(5, 5, 5);
+  
+  return 0;
+}
