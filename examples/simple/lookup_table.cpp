d8b501ffeffddcc4eb3434b311f0ab4ad3ab291d
Richard C <richard.calland@ipmu.jp>
2016-07-17 23:20:06 +0900

    compileable state, mostly focussed on host code.

--- examples/simple/lookup_table.cpp
+++ examples/simple/lookup_table.cpp
@@ -1,12 +1,51 @@
 #include <stdio.h>
 #include "hemi/hemi.h"
+#include "hemi/array.h"
 #include "hemi/table.h"
 #include "hemi/launch.h"
+#include "hemi/parallel_for.h"
 #include "hemi/device_api.h"
+
+void lookup(const int n, float *val, const hemi::table3D<float> lookupTable, const float x, const float y, const float z)
+{
+  hemi::parallel_for(0, n, [&] HEMI_LAMBDA (int i) {
+      val[i] = lookupTable.lookup(1.0, 1.0, 1.0);
+    });
+}
+
+inline int index(int x, int y, int z, int dx, int dy)
+{
+  return x + dx * (y + dy*z);
+}
 
 int main(void) {
 
-  hemi::Table3D<float> lookup_table(5, 5, 5);
+  const int n = 1000;
+  const int nx = 5, ny = 5, nz = 5;
+
+  float *table_array = new float[nx*ny*nz];
+
+  // lets make some test data
+  for (int z = 0; z < nz; ++z)
+    for (int y = 0; y < ny; ++y)
+      for (int x = 0; x < nx; ++x)
+	table_array[hemi::index(x,y,z,nx,ny)] = 0.5f;
+
+  hemi::Array<float> output(n);
+  hemi::Table3D<float> lookup_table(nx, ny, nz, table_array);
   
+  //hemi::table3D<float> honk;
+  //printf("%f", honk.lookup(1,1,1));
+
+  //const hemi::table3D<float> tbl = lookup_table.readOnlyTable();
+  //printf("%f/n", tbl.lookup(1.0, 1.0, 1.0));
+  
+  //printf("%f", lookup_table.readOnlyTable().lookup(1,1,1));
+  
+  lookup(n, output.hostPtr(), lookup_table.readOnlyTable(), 2.5, 2.5, 2.5);
+  printf("element 0 = %f\n", output.hostPtr()[0]);
+
+  //delete [] table_array;
+
   return 0;
 }
--- hemi/table.h
+++ hemi/table.h
@@ -28,176 +28,222 @@
 
 namespace hemi {
 
-	//template <typename T> class Table1D; // forward decl
-	//template <typename T> class Table2D; // forward decl
-	template <typename T> class Table3D; // forward decl
+  inline int index(int x, int y, int z, int dx, int dy)
+  {
+    return x + dx * (y + dy*z);
+  }
 
-    // EDIT: this idea wont work!
-	// move these typedefs to hemi.h maybe.
-	// http://tipsandtricks.runicsoft.com/Cpp/MemberFunctionPointers.html
-	//typedef float ( hemi::Table3D::*table ) (float, float, float);
-
-	template <typename T>
-	  struct table3D {
-
-	    HEMI_DEV_CALLABLE_INLINE_MEMBER T lookup(const T x, const T y, const T z) const
-	    {
+  
+  //template <typename T> class Table1D; // forward decl
+  //template <typename T> class Table2D; // forward decl
+  template <typename T> class Table3D; // forward decl
+  
+  // EDIT: this idea wont work!
+  // move these typedefs to hemi.h maybe.
+  // http://tipsandtricks.runicsoft.com/Cpp/MemberFunctionPointers.html
+  //typedef float ( hemi::Table3D::*table ) (float, float, float);
+  
+  template <typename T>
+    struct table3D {
+      
+      HEMI_DEV_CALLABLE_INLINE_MEMBER T lookup(const T x, const T y, const T z) const
+      {
 #ifdef HEMI_DEV_CODE
-	      return tex3D<T>(texture, x ,y ,z);
+	return tex3D<T>(texture, x, y, z);
 #else
-	      return 1.0;
+	T xd = x;//(x - xbound[0]) * inv_cell_size[0];
+	T yd = y;// (y - ybound[0]) * inv_cell_size[1];
+	T zd = z;//(z - zbound[0]) * inv_cell_size[2];
+
+	int ubx = static_cast<int>(xd);
+	int uby = static_cast<int>(yd);
+	int ubz = static_cast<int>(zd);
+
+	int obx = ubx + 1;
+	int oby = uby + 1;
+	int obz = ubz + 1;
+
+	const T v[] = { hPtr[hemi::index(ubx, uby, ubz, sizeX, sizeY)], hPtr[hemi::index(ubx, uby, obz, sizeX, sizeY)],
+			hPtr[hemi::index(ubx, oby, ubz, sizeX, sizeY)], hPtr[hemi::index(ubx, oby, obz, sizeX, sizeY)],
+			hPtr[hemi::index(obx, uby, ubz, sizeX, sizeY)], hPtr[hemi::index(obx, uby, obz, sizeX, sizeY)],
+			hPtr[hemi::index(obx, oby, ubz, sizeX, sizeY)], hPtr[hemi::index(obx, oby, obz, sizeX, sizeY)] };
+	
+	xd -= (float)ubx;
+	yd -= (float)uby;
+	zd -= (float)ubz;
+  
+	float i1 = v[0] * (1 - zd) + v[1] * zd;
+	float i2 = v[2] * (1 - zd) + v[3] * zd;
+	float j1 = v[4] * (1 - zd) + v[5] * zd;
+	float j2 = v[6] * (1 - zd) + v[7] * zd;
+  
+	float w1 = i1 * (1 - yd) + i2 * yd;
+	float w2 = j1 * (1 - yd) + j2 * yd;
+
+	float result = w1 * (1 - xd) + w2 * xd;
+	return result;
 #endif
-	    };
-
-	    #ifndef HEMI_CUDA_DISABLE
-	    cudaTextureObject_t texture;
-	    #endif
-	    mutable T *hPtr;
-	  };
-
-	enum Location {
-        host   = 0,
-        device = 1
+      };
+      
+#ifndef HEMI_CUDA_DISABLE
+      cudaTextureObject_t texture;
+#endif
+      mutable T *hPtr;
+      int sizeX;
+      int sizeY; 
+      int sizeZ;
     };
-
-    template <typename T>
+  
+  // already declared in array.h
+  /*enum Location {
+    host   = 0,
+    device = 1
+    };*/
+  
+  template <typename T>
     class Table3D 
     {
     public:
-    	Table3D(size_t nx, size_t ny, size_t nz) :
-    	nSize(nx * ny * nz);
-    	nSizeX(nx);
-    	nSizeY(ny);
-    	nSizeZ(nz);
-    	{	
-    	}
+    Table3D(size_t nx, size_t ny, size_t nz, T *data) :
+      nSize(nx * ny * nz),
+	nSizeX(nx),
+	nSizeY(ny),
+	nSizeZ(nz)
+	{	
+	  // this is unsafe!
+	  _table.hPtr = data;
+	  _table.sizeX = nx;
+	  _table.sizeY = ny;
+	  _table.sizeZ = nz;
 
-    	~Table3D()
-    	{
-    		deallocateDevice();
-            deallocateHost();
-    	}
-
-    	table getTable() const
-    	{
+	  isHostAlloced = true;
+	}
+      
+      ~Table3D()
+	{
+	  deallocateDevice();
+	  deallocateHost();
+	}
+      
+      const table3D<T> readOnlyTable() const
+	{
 	  return _table;
-    	}
-
+	}
+      
     private:
-    	size_t nSize;
-    	size_t nSizeX;
-    	size_t nSizeY;
-    	size_t nSizeZ;
-
-    	cudaArray *dPtr;
-        
-	cudaMemcpy3DParms copyParams;
-	cudaExtent volumeSize;
-	table _table;
-
-        mutable bool    isHostAlloced;
-        mutable bool    isDeviceAlloced;        
-
-        mutable bool    isHostValid;
-        mutable bool    isDeviceValid;
-
+      size_t nSize;
+      size_t nSizeX;
+      size_t nSizeY;
+      size_t nSizeZ;
+      
+#ifndef HEMI_CUDA_DISABLE
+      cudaArray *dPtr;
+      
+      cudaMemcpy3DParms copyParams;
+      cudaExtent volumeSize;
+#endif
+      table3D<T> _table;
+      
+      mutable bool    isHostAlloced;
+      mutable bool    isDeviceAlloced;        
+      
+      mutable bool    isHostValid;
+      mutable bool    isDeviceValid;
+      
     protected:
-        void allocateHost() const
-        {
-            assert(!isHostAlloced);
-            _table.hPtr = new T[nSize];    
-                
-            isHostAlloced = true;
-            isHostValid = false;
-        }
-
-    	void allocateDevice() const
-    	{
+      void allocateHost() const
+      {
+	assert(!isHostAlloced);
+	_table.hPtr = new T[nSize];    
+	
+	isHostAlloced = true;
+	isHostValid = false;
+      }
+      
+      void allocateDevice() const
+      {
 #ifndef HEMI_CUDA_DISABLE
-    		assert(!isDeviceAlloced);
-    	    volumeSize = make_cudaExtent(nSizeX, nSizeY, nSizeZ);
-  			cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<T>();
-  			cudaMalloc3DArray(dPtr, &channelDesc, volumeSize);
-			copyParams.dstArray = dPtr;
-
-    	    isDeviceAlloced = true;
-            isDeviceValid = false;
+	assert(!isDeviceAlloced);
+	volumeSize = make_cudaExtent(nSizeX, nSizeY, nSizeZ);
+	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<T>();
+	cudaMalloc3DArray(dPtr, &channelDesc, volumeSize);
+	copyParams.dstArray = dPtr;
+	
+	isDeviceAlloced = true;
+	isDeviceValid = false;
 #endif
-    	}
-
-        void deallocateHost()
-        {
-            if (isHostAlloced) {
-                delete [] _table.hPtr;
-                nSize = 0;
-                nSizeX = 0;
-                nSizeY = 0;
-                nSixeZ = 0;
-                isHostAlloced = false;
-                isHostValid   = false;
-            }
-        }
-
-        void deallocateDevice()
-        {
+      }
+      
+      void deallocateHost()
+      {
+	if (isHostAlloced) {
+	  delete [] _table.hPtr;
+	  nSize = 0;
+	  nSizeX = 0;
+	  nSizeY = 0;
+	  nSizeZ = 0;
+	  isHostAlloced = false;
+	  isHostValid   = false;
+	}
+      }
+      
+      void deallocateDevice()
+      {
 #ifndef HEMI_CUDA_DISABLE
-            if (isDeviceAlloced) {
-                checkCuda( cudaFreeArray(dPtr) );
-                checkCuda( cudaDestroyTextureObject(_table.texture) );
-                isDeviceAlloced = false;
-                isDeviceValid   = false;
-            }
+	if (isDeviceAlloced) {
+	  checkCuda( cudaFreeArray(dPtr) );
+	  checkCuda( cudaDestroyTextureObject(_table.texture) );
+	  isDeviceAlloced = false;
+	  isDeviceValid   = false;
+	}
 #endif
-        }
-
-        void copyHostToDevice() const
-        {
+      }
+      
+      void copyHostToDevice() const
+      {
 #ifndef HEMI_CUDA_DISABLE
-            assert(isHostAlloced);
-            if (!isDeviceAlloced) allocateDevice();
-            copyParams.srcPtr = make_cudaPitchedPtr((void*)_table.hPtr, volumeSize.width * sizeof(float), volumeSize.width, volumeSize.height);
-	    copyParams.extent   = volumeSize;
-	    copyParams.kind     = cudaMemcpyHostToDevice;
-	    cudaMemcpy3D(&copyParams);
-	    
-	    // bind to texture
-	    struct cudaResourceDesc resDesc;
-	    memset(&resDesc, 0, sizeof(resDesc));
-	    resDesc.resType =  cudaResourceTypeArray;
-	    resDesc.res.linear.devPtr = dPtr;
-	    resDesc.res.linear.sizeInBytes = volumeSize.width * volumeSize.height * volumeSize.depth * sizeof(T);
-	    resDesc.res.linear.desc.f = cudaChannelFormatKindFloat;
-	    resDesc.res.linear.desc.x = sizeof(T) * 8;
-	    struct cudaTextureDesc texDesc;
-	    memset(&texDesc, 0, sizeof(texDesc));
-	    texDesc.normalizedCoords = true;
-	    texDesc.readMode = cudaReadModeElementType;
-	    texDesc.filterMode = cudaFilterModeLinear;
-	    
-	    texDesc.addressMode[0] = cudaAddressModeClamp;
-	    texDesc.addressMode[1] = cudaAddressModeClamp;
-	    texDesc.addressMode[2] = cudaAddressModeClamp;
-	    
-	    cudaCreateTextureObject(&_table.texture, &resDesc, &texDesc, NULL);
-	    
-            isDeviceValid = true;
+	assert(isHostAlloced);
+	if (!isDeviceAlloced) allocateDevice();
+	copyParams.srcPtr = make_cudaPitchedPtr((void*)_table.hPtr, volumeSize.width * sizeof(float), volumeSize.width, volumeSize.height);
+	copyParams.extent   = volumeSize;
+	copyParams.kind     = cudaMemcpyHostToDevice;
+	cudaMemcpy3D(&copyParams);
+	
+	// bind to texture
+	struct cudaResourceDesc resDesc;
+	memset(&resDesc, 0, sizeof(resDesc));
+	resDesc.resType =  cudaResourceTypeArray;
+	resDesc.res.linear.devPtr = dPtr;
+	resDesc.res.linear.sizeInBytes = volumeSize.width * volumeSize.height * volumeSize.depth * sizeof(T);
+	resDesc.res.linear.desc.f = cudaChannelFormatKindFloat;
+	resDesc.res.linear.desc.x = sizeof(T) * 8;
+	struct cudaTextureDesc texDesc;
+	memset(&texDesc, 0, sizeof(texDesc));
+	texDesc.normalizedCoords = true;
+	texDesc.readMode = cudaReadModeElementType;
+	texDesc.filterMode = cudaFilterModeLinear;
+	
+	texDesc.addressMode[0] = cudaAddressModeClamp;
+	texDesc.addressMode[1] = cudaAddressModeClamp;
+	texDesc.addressMode[2] = cudaAddressModeClamp;
+	
+	cudaCreateTextureObject(&_table.texture, &resDesc, &texDesc, NULL);
+	
+	isDeviceValid = true;
 #endif
-        }
-
-        void copyDeviceToHost() const
-        {
-/*#ifndef HEMI_CUDA_DISABLE
-            assert(isDeviceAlloced);
-            if (!isHostAlloced) allocateHost();
-            checkCuda( cudaMemcpy(hPtr, 
-                                  dPtr, 
-                                  nSize * sizeof(T), 
-                                  cudaMemcpyDeviceToHost) );
-            isHostValid = true;
-#endif*/
-        }
-
+      }
+      
+      void copyDeviceToHost() const
+      {
+	/*#ifndef HEMI_CUDA_DISABLE
+	  assert(isDeviceAlloced);
+	  if (!isHostAlloced) allocateHost();
+	  checkCuda( cudaMemcpy(hPtr, 
+	  dPtr, 
+	  nSize * sizeof(T), 
+	  cudaMemcpyDeviceToHost) );
+	  isHostValid = true;
+	  #endif*/
+      }
     };
-
-
 }
