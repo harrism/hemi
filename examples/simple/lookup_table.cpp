2518c047f013ed0707484a3612b5c5dd890697ec
Richard C <richard.calland@ipmu.jp>
2016-07-24 18:42:37 +0900

    benchmarking, and some housekeeping. also added the constructor for not using a foreign pointer

--- examples/simple/lookup_table.cpp
+++ examples/simple/lookup_table.cpp
@@ -4,6 +4,7 @@
 #include "hemi/array.h"
 #include "hemi/table.h"
 //#include "hemi/launch.h"
+#include "hemi/grid_stride_range.h"
 #include "hemi/parallel_for.h"
 #include "hemi/device_api.h"
 
@@ -14,50 +15,72 @@ void lookup(const int n, float *val, const hemi::table3<float> lookupTable, cons
     });
 }
 
-float lookup_validate(const int n, hemi::Array<float> *output,
-		      const hemi::table3<float> lookupTable) 
+HEMI_LAUNCHABLE void lookup_kernel(const int n, float *output, const hemi::table3<float> lookupTable, const float *x, const float *y, const float *z)
 {
+  for (auto i : hemi::grid_stride_range(0, n)) {
+    output[i] = lookupTable.lookup(x[i], y[i], z[i]);
+  }
+}
+
+float lookup_validate(const int n, const hemi::table3<float> lookupTable) 
+{
+  hemi::Array<float> output_gpu(n);
+  hemi::Array<float> output_cpu(n);
+
   hemi::Array<float> x_val(n);
   hemi::Array<float> y_val(n);
   hemi::Array<float> z_val(n);
   
-  //std::srand(0);
-  
   for (int i = 0; i < n; ++i) {
-    x_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[0]-1) / (float)(lookupTable.inv_cell_size[0]));
-    y_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[1]-1) / (float)(lookupTable.inv_cell_size[1]));
-    z_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[2]-1) / (float)(lookupTable.inv_cell_size[2]));
-    //printf("random sample %i = [%f, %f, %f]\n", i, x_val.readOnlyHostPtr()[i], y_val.readOnlyHostPtr()[i], z_val.readOnlyHostPtr()[i]);
+    x_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[0]-1) / (float)(lookupTable.reciprocal_cell_size[0]));
+    y_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[1]-1) / (float)(lookupTable.reciprocal_cell_size[1]));
+    z_val.writeOnlyHostPtr()[i] = (std::rand() / (float)RAND_MAX) * ( (lookupTable.size[2]-1) / (float)(lookupTable.reciprocal_cell_size[2]));
   }
 
-  float *dev_ptr = output->writeOnlyDevicePtr();
+  float *dev_ptr = output_gpu.writeOnlyDevicePtr();
   const float *x_ptr = x_val.readOnlyDevicePtr();
   const float *y_ptr = y_val.readOnlyDevicePtr();
   const float *z_ptr = z_val.readOnlyDevicePtr();
 
-  hemi::parallel_for(0, n, [=] HEMI_LAMBDA (int i) {
+  // calculate GPU interpolations
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start);
+
+  /*hemi::parallel_for(0, n, [lookupTable, x_ptr, y_ptr, z_ptr, dev_ptr] HEMI_LAMBDA (int i) {
       dev_ptr[i] = lookupTable.lookup(x_ptr[i], y_ptr[i], z_ptr[i]);
-      //if (i == 5000) {
-      //printf("GPU %i %f\n", i, dev_ptr[i]);
-	//printf("GPU element %i %f\n", 0, lookupTable.getElement(1,0,0));
-	// }
-    });
+      });*/
+  hemi::cudaLaunch(lookup_kernel, n, output_gpu.writeOnlyDevicePtr(), lookupTable, x_ptr, y_ptr, z_ptr);
+
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+  float milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
+  printf("Time for %i lookups on GPU was %f ms\n", n, milliseconds);
+
+  
+  // compare to CPU result
+  cudaEvent_t start2, stop2;
+  cudaEventCreate(&start2);
+  cudaEventCreate(&stop2);
+  cudaEventRecord(start2);
   
   for (int i = 0; i < n; ++i) {
-    float value = lookupTable.lookup(x_val.readOnlyHostPtr()[i],
-				     y_val.readOnlyHostPtr()[i],
-				     z_val.readOnlyHostPtr()[i]);
-    // if (i == 5000) {
-    //printf("CPU %i %f [%f,%f,%f]\n", i, value, x_val.readOnlyHostPtr()[i], y_val.readOnlyHostPtr()[i], z_val.readOnlyHostPtr()[i]);
-      //printf("CPU element %i %f\n", 0, lookupTable.getElement(2,5,1));
-      // }
-    output->hostPtr()[i] -= value;
-    output->hostPtr()[i] /= value;
+    output_cpu.hostPtr()[i] = lookupTable.lookup(x_val.readOnlyHostPtr()[i],
+						 y_val.readOnlyHostPtr()[i],
+						 z_val.readOnlyHostPtr()[i]);
   }
 
-  float average = 0.0f;
+  cudaEventRecord(stop2);
+  cudaEventSynchronize(stop2);
+  milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start2, stop2);
+  printf("Time for %i lookups on CPU was %f ms\n", n, milliseconds);
+  
+  float average = 0;
   for (int i = 0; i < n; ++i) {
-    average += output->hostPtr()[i];
+    average += (output_gpu.hostPtr()[i] - output_cpu.hostPtr()[i]) / output_cpu.hostPtr()[i];
   }
 
   average /= (float)n;
@@ -67,23 +90,20 @@ float lookup_validate(const int n, hemi::Array<float> *output,
 int main(void) {
   std::srand(0);
 
-  const int n = 100;
-  const int nx = 25, ny = 25, nz = 25;
-
-  float *table_array = new float[nx*ny*nz];
-  
-  // lets make some test data
-  for (int z = 0; z < nz; ++z)
-    for (int y = 0; y < ny; ++y)
-      for (int x = 0; x < nx; ++x)
-	table_array[hemi::index(x, y, z, nx, ny)] = x;// + y + z;// (std::rand() * 1.0) / (float)RAND_MAX;
+  const int n = 1000000;
+  const int nx = 100, ny = 100, nz = 100;
 
   hemi::Array<float> output(n);
-  hemi::Table3D<float> lookup_table(table_array, 
-				    nx, ny, nz, 
+  hemi::Table3D<float> lookup_table(nx, ny, nz, 
 				    0.0, 0.0, 0.0,
 				    100.0, 100.0, 100.0);
   
+  // lets make some test data                                                                                              
+  for (int z = 0; z < nz; ++z)
+    for (int y = 0; y < ny; ++y)
+      for (int x = 0; x < nx; ++x)
+        lookup_table.writeOnlyHostPtr()[hemi::index(x, y, z, nx, ny)] = std::rand() / (float)RAND_MAX;
+
   /*  float *pointer;
 #ifndef HEMI_CUDA_DISABLE
   pointer = output.devicePtr();
@@ -95,7 +115,7 @@ int main(void) {
   
   //printf("element 0 = %f\n", output.hostPtr()[0]);
 
-  float precision = lookup_validate(n, &output, lookup_table.readOnlyTable());
+  float precision = lookup_validate(n, lookup_table.readOnlyTable());
   printf("precision %f\n", precision);
   //delete [] table_array;
 
--- hemi/table.h
+++ hemi/table.h
@@ -20,16 +20,22 @@
 #include <climits>
 #include <type_traits> 
 
-#ifndef HEMI_ARRAY_DEFAULT_LOCATION
+/*#ifndef HEMI_ARRAY_DEFAULT_LOCATION
   #ifdef HEMI_CUDA_COMPILER
     #define HEMI_ARRAY_DEFAULT_LOCATION hemi::device
   #else
     #define HEMI_ARRAY_DEFAULT_LOCATION hemi::host
   #endif
-#endif
+  #endif*/
 
 namespace hemi {
 
+  /* flatten a 2D array index into 1D */
+  HEMI_DEV_CALLABLE_INLINE int index(const int x, const int y, const int dx) {
+    return x + dx * y;
+  }
+
+  /* flatten a 3D array index into 1D */
   HEMI_DEV_CALLABLE_INLINE int index(const int x, const int y, const int z, const int dx, const int dy) {
     return x + dx * (y + dy * z);
   }
@@ -38,54 +44,50 @@ namespace hemi {
   //template <typename T> class Table2D; // forward decl
   template <typename T> class Table3D; // forward decl
   
+  /* this is essentially a wrapper for cudaTextureObject, which should be passed by value to the kernel */
   template <typename T>
     struct table3 {      
+      /* look up element value, i.e. no interpolation between cells is applied */
       HEMI_DEV_CALLABLE_INLINE_MEMBER T getElement(const int i, const int j, const int k) const
       {
 #ifdef HEMI_DEV_CODE
-        return tex3D<T>(texture, (T)i + 0.5f, (T)j + 0.5f, (T)k + 0.5f);
+        return tex3D<T>(texture, (float)i + 0.5f, (float)j + 0.5f, (float)k + 0.5f);
 #else
 	return hPtr[hemi::index(i, j, k, size[0], size[1])];
 #endif
-	
       };
       
-      HEMI_DEV_CALLABLE_INLINE_MEMBER T lookup(const T x, const T y, const T z) const
+      /* lookup function, linearly interpolates between cells */
+      HEMI_DEV_CALLABLE_INLINE_MEMBER T lookup(const float x, const float y, const float z) const
       {
-#ifdef HEMI_DEV_CODE
-
-	T xd = 0.5 + (x - low_edge[0]) * ((size[0]-1) / (up_edge[0] - low_edge[0]));//inv_cell_size[0];
-	T yd = 0.5 + (y - low_edge[1]) * ((size[1]-1) / (up_edge[1] - low_edge[1]));//inv_cell_size[1];
-	T zd = 0.5 + (z - low_edge[2]) * ((size[2]-1) / (up_edge[2] - low_edge[2]));//inv_cell_size[2];
-
-	return tex3D<T>(texture, xd, yd, zd);
-#else
-	T xd = (x - low_edge[0]) * inv_cell_size[0];
-        T yd = (y - low_edge[1]) * inv_cell_size[1];
-        T zd = (z - low_edge[2]) * inv_cell_size[2];
+	float xd = (x - low_edge[0]) * reciprocal_cell_size[0];
+        float yd = (y - low_edge[1]) * reciprocal_cell_size[1];
+        float zd = (z - low_edge[2]) * reciprocal_cell_size[2];
 	
-	int ubx = (int)xd;//static_cast<int>(xd);
-	int uby = (int)yd;//static_cast<int>(yd);
-	int ubz = (int)zd;//static_cast<int>(zd);
+#ifdef HEMI_DEV_CODE
+	return tex3D<T>(texture, xd + 0.5f, yd + 0.5f, zd + 0.5f);
+#else
+	int ubx = (int)xd;
+	int uby = (int)yd;
+	int ubz = (int)zd;
 	
 	int obx = ubx + 1;
 	int oby = uby + 1;
 	int obz = ubz + 1;
-	
-	const float v[] = { hPtr[hemi::index(ubx, uby, ubz, size[0], size[1])], hPtr[hemi::index(ubx, uby, obz, size[0], size[1])],
-			    hPtr[hemi::index(ubx, oby, ubz, size[0], size[1])], hPtr[hemi::index(ubx, oby, obz, size[0], size[1])],
-			    hPtr[hemi::index(obx, uby, ubz, size[0], size[1])], hPtr[hemi::index(obx, uby, obz, size[0], size[1])],
-			    hPtr[hemi::index(obx, oby, ubz, size[0], size[1])], hPtr[hemi::index(obx, oby, obz, size[0], size[1])] };
-	
-	xd -= static_cast<float>(ubx);
-	yd -= static_cast<float>(uby);
-	zd -= static_cast<float>(ubz);
-  
-	float i1 = v[0] * (1.0f - zd) + v[1] * zd;
-	float i2 = v[2] * (1.0f - zd) + v[3] * zd;
-	float j1 = v[4] * (1.0f - zd) + v[5] * zd;
-	float j2 = v[6] * (1.0f - zd) + v[7] * zd;
-  
+
+	xd -= std::floor(ubx);
+	yd -= std::floor(uby);
+	zd -= std::floor(ubz);
+
+	float i1 = hPtr[hemi::index(ubx, uby, ubz, size[0], size[1])] * (1.0f - zd) 
+	  + hPtr[hemi::index(ubx, uby, obz, size[0], size[1])] * zd;
+        float i2 = hPtr[hemi::index(ubx, oby, ubz, size[0], size[1])] * (1.0f - zd) 
+	  + hPtr[hemi::index(ubx, oby, obz, size[0], size[1])] * zd;
+        float j1 = hPtr[hemi::index(obx, uby, ubz, size[0], size[1])] * (1.0f - zd) 
+	  + hPtr[hemi::index(obx, uby, obz, size[0], size[1])] * zd;
+	float j2 = hPtr[hemi::index(obx, oby, ubz, size[0], size[1])] * (1.0f - zd) 
+	  + hPtr[hemi::index(obx, oby, obz, size[0], size[1])] * zd;
+
 	float w1 = i1 * (1.0f - yd) + i2 * yd;
 	float w2 = j1 * (1.0f - yd) + j2 * yd;
 
@@ -100,22 +102,53 @@ namespace hemi {
       
       int size[3];
       float low_edge[3];
-      float up_edge[3];
-      float inv_cell_size[3];
+      float reciprocal_cell_size[3];
     };
   
   template <typename T>
     class Table3D 
     {
     public:
+      Table3D(size_t nx, size_t ny, size_t nz,
+	      float low_edge_x, float low_edge_y, float low_edge_z,
+	      float up_edge_x, float up_edge_y, float up_edge_z) 
+	{
+	  _table.size[0] = nx;
+	  _table.size[1] = ny;
+	  _table.size[2] = nz;
+	  
+	  // set the table range                                                                                                                                        
+          _table.low_edge[0] = low_edge_x;
+          _table.low_edge[1] = low_edge_y;
+          _table.low_edge[2] = low_edge_z;
+
+	  // width of each cell                                                                                                   
+          _table.reciprocal_cell_size[0] = (nx-1) / static_cast<float>(up_edge_x - low_edge_x);
+          _table.reciprocal_cell_size[1] = (ny-1) / static_cast<float>(up_edge_y - low_edge_y);
+          _table.reciprocal_cell_size[2] = (nz-1) / static_cast<float>(up_edge_z - low_edge_z);
+
+	  normalized_coords = false;
+	  isHostAlloced = false;
+	  isDeviceAlloced = false;
+	  isHostValid = false;
+	  isDeviceValid = false;
+	  isForeignHostPtr = false;
+
+	  allocateHost();
+
+#ifndef HEMI_CUDA_DISABLE
+	  allocateDevice();
+#endif
+	}
+
     Table3D(T *data,
 	    size_t nx, size_t ny, size_t nz,
 	    float low_edge_x, float low_edge_y, float low_edge_z,
-	    float up_edge_x, float up_edge_y, float up_edge_z) :
+	    float up_edge_x, float up_edge_y, float up_edge_z) /*:
       nSize(nx * ny * nz),
 	nSizeX(nx),
 	nSizeY(ny),
-	nSizeZ(nz)
+	nSizeZ(nz)*/
 	{	
 	  // this is unsafe!
 	  _table.hPtr = data;
@@ -127,20 +160,23 @@ namespace hemi {
 	  _table.low_edge[0] = low_edge_x;
 	  _table.low_edge[1] = low_edge_y;
 	  _table.low_edge[2] = low_edge_z;
-	  _table.up_edge[0] = up_edge_x;
+	  /*_table.up_edge[0] = up_edge_x;
 	  _table.up_edge[1] = up_edge_y;
-          _table.up_edge[2] = up_edge_z;
+          _table.up_edge[2] = up_edge_z;*/
 
 	  // width of each cell
-	  _table.inv_cell_size[0] = (nx-1) / static_cast<float>(up_edge_x - low_edge_x);
-	  _table.inv_cell_size[1] = (ny-1) / static_cast<float>(up_edge_y - low_edge_y);
-          _table.inv_cell_size[2] = (nz-1) / static_cast<float>(up_edge_z - low_edge_z);
+	  _table.reciprocal_cell_size[0] = (nx-1) / static_cast<float>(up_edge_x - low_edge_x);
+	  _table.reciprocal_cell_size[1] = (ny-1) / static_cast<float>(up_edge_y - low_edge_y);
+          _table.reciprocal_cell_size[2] = (nz-1) / static_cast<float>(up_edge_z - low_edge_z);
 
+	  isForeignHostPtr = true;
 	  isHostAlloced = true;
 	  isHostValid = true;
 	  isDeviceAlloced = false;
 	  isDeviceValid = false;
-	  
+
+	  // no need for this as we are using a foreign pointer
+	  //allocateHost(); 
 #ifndef HEMI_CUDA_DISABLE
 	  allocateDevice();
 	  isDeviceAlloced = true;
@@ -155,16 +191,31 @@ namespace hemi {
 	  deallocateHost();
 	}
       
-      const table3<T> readOnlyTable() const
-	{
-	  return _table;
-	}
+      const table3<T> readOnlyTable()
+      {
+	/* try copying the struct to device too */
+#ifndef HEMI_CUDA_DISABLE
+	assert(isDeviceAlloced);
+	if (!isDeviceValid)
+	  copyHostToDevice();
+#endif
+	
+	return _table;
+      }
+
+      T* writeOnlyHostPtr()
+      {
+	if (!isHostAlloced) allocateHost();
+	isDeviceValid = false;
+	isHostValid   = true;
+	return _table.hPtr;
+      }
       
     private:
-      size_t nSize;
+      /*size_t nSize;
       size_t nSizeX;
       size_t nSizeY;
-      size_t nSizeZ;
+      size_t nSizeZ;*/
       
 #ifndef HEMI_CUDA_DISABLE
       cudaArray_t dPtr;
@@ -172,6 +223,8 @@ namespace hemi {
 #endif
       table3<T> _table;
       
+      bool            isForeignHostPtr;
+
       mutable bool    isHostAlloced;
       mutable bool    isDeviceAlloced;        
       
@@ -184,7 +237,8 @@ namespace hemi {
       void allocateHost() const
       {
 	assert(!isHostAlloced);
-	_table.hPtr = new T[nSize];    
+	
+	_table.hPtr = new T[_table.size[0] * _table.size[1] * _table.size[2]];    
 	
 	isHostAlloced = true;
 	isHostValid = false;
@@ -194,7 +248,7 @@ namespace hemi {
       {
 #ifndef HEMI_CUDA_DISABLE
 	assert(!isDeviceAlloced);
-	volumeSize = make_cudaExtent(nSizeX, nSizeY, nSizeZ);
+	volumeSize = make_cudaExtent(_table.size[0], _table.size[1], _table.size[2]);
 	cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<T>();
 	checkCuda( cudaMalloc3DArray(&dPtr, &channelDesc, volumeSize) );
 	
@@ -206,11 +260,9 @@ namespace hemi {
       void deallocateHost()
       {
 	if (isHostAlloced) {
-	  //delete [] _table.hPtr;
-	  nSize  = 0;
-	  nSizeX = 0;
-	  nSizeY = 0;
-	  nSizeZ = 0;
+	  if (!isForeignHostPtr)
+	    delete [] _table.hPtr;
+
 	  isHostAlloced = false;
 	  isHostValid   = false;
 	}
